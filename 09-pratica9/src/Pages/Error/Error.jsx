ca537dfa2a1b39ef2676b21316068d0b58dc7fac
Cristhian Nolazco <nolazcocristhian@gmail.com>
2025-05-29 23:23:17 +0200

    Keep calm and commit

---
+++ 09-pratica9/src/Pages/Error/Error.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Error</h1>
+            <Link to='/'>Voltar</Link>
+        </div>
+    );
+};
+
+export default Error;
